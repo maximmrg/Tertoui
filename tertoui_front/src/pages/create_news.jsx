569b928b50c8991dba8a4eeec4eb5ccef8443346
Maxime Marigliano <maxim.marigliano@gmail.com>
2022-04-03 19:55:12 +0200

    first forms

---
+++ tertoui_front/src/pages/create_news.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { Component } from 'react';
+import ReactDOM from 'react-dom'
+
+
+class Field extends Component {
+
+    render () {
+        const {name, value, onChange} = this.props
+        return <div className="form-group">
+            <label htmlFor={name}>Titre</label>
+            <input type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
+        </div>
+    }
+}
+
+class TextAreaField extends Component {
+    render () {
+        const {name, value, onChange, children} = this.props
+        return <div className="form-group">
+            <label htmlFor={name}>{children}</label>
+            <textarea type="text" value={value} onChange={onChange} id={name} name={name} className="form-control"/>
+        </div>
+    }
+}
+
+export class NewsForm extends Component {
+
+    constructor (props) {
+        super(props)
+        this.state = {
+            titre: '',
+            article: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange (e) {
+        console.log(e)
+        const name = e.target.name
+        this.setState({
+            [name]: e.target.value
+        })
+    }
+
+    render () {
+        return <div className='container'>
+            <h1>Votre Actualité</h1>
+            <Field name="titre" value={this.state.titre} onChange={this.handleChange}>Titre</Field>
+            <TextAreaField name="article" value={this.state.article} onChange={this.handleChange}>Article</TextAreaField>
+        </div>
+    }
+}
