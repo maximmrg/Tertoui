b67d410f5461e5018ee2aec96da8122506d98990
Maxime Marigliano <maxim.marigliano@gmail.com>
2022-04-06 19:07:40 +0200

    test call api

--- tertoui_front/src/pages/index.jsx
+++ tertoui_front/src/pages/index.jsx
@@ -5,20 +5,50 @@ import NewsCarousel from '../components/NewsCarousel';
 
 export class Index extends Component {
 
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            test : ''
+        }
+    }
+
+    testApi() {
+        fetch("https://baconipsum.com/api/?type=meat-and-filler", {
+            "method": "GET",
+            "headers": {
+                
+            }
+        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+                this.setState({
+                    test: response
+                })                
+            })
+            .catch(err => {
+                console.log(err);
+            });
+    }
+
     render() {
-       return <div className="App">
+        return <div className="App">
             <h1>Tertoui</h1>
 
             <h3>LE site d'actualité</h3>
+
+            <button className='btn btn-warning' onClick={this.testApi.bind(this)}>Test API</button>
+            <br/>
             <Link to='/login'>Se connecter</Link>
             <br />
             <Link to='/logon'>Créer compte</Link>
             <br />
             <Link to='/news_form'>Créer une news</Link>
-            <br/>
+            <br />
             <Link to='/news'>News</Link>
-            <NewsCarousel/>
-        </div> 
+            <NewsCarousel />
+        </div>
     }
 };
 
